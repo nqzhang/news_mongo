360361df59e05dc6b194ab2287f5b03b09d7e861
kiwi <242637385@qq.com>
2018-10-25 02:28:58 +0800

    添加数据库的文章 tags、category join 实现

--- models/join.py
+++ models/join.py
@@ -1,3 +1,4 @@
+#传入的posts是多个post的列表
 async def post_user(posts,db):
     users = [i['user'] for i in posts]
     users = db.users.find({'_id': {'$in': users}})
@@ -10,3 +11,24 @@ async def post_user(posts,db):
         post['post_date'] = post['post_date'].strftime("%Y-%m-%d %H:%M")
         posts_new.append(post)
     return posts_new
+
+#传入的posts是单个post的字典
+async def post_tags(post,db):
+    tags = [x for x in post['tags']]
+    tags = db.terms.find({'_id': {'$in': tags}})
+    tag_dict = {}
+    async for tag in tags:
+        tag_dict[tag['_id']] = tag
+    post['tags'] = [tag_dict[i] for i in post['tags']]
+    return post
+
+#传入的posts是单个post的字典
+async def post_category(post,db):
+    category = [x for x in post['category']]
+    category = db.terms.find({'_id': {'$in': category}})
+    category_dict = {}
+    async for category in category:
+        category_dict[category['_id']] = category
+    post['category'] = [category_dict[i] for i in post['category']]
+    return post
+
