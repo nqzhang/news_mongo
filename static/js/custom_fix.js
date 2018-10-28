eba7936fbe5a12c70c70274a9d45af60a8e3baff
kiwi <242637385@qq.com>
2018-10-29 01:30:30 +0800

    修复采集来的文章代码样式

--- static/js/custom_fix.js
+++ static/js/custom_fix.js
@@ -1,5 +1,7 @@
 $(function() {
     $('.index-middle').after($('.right_sidebar'));
     $('a[href^="http"]').not('a[href*=smwenku\\.com]').attr('target','_blank');
+    $('a[href^="http"]').not('a[href*=smwenku\\.com]').attr('target','_blank');
+    $('article-content pre.cpp').removeClass('cpp').addClass('language-cpp');
 });
 
