1056601a0c0b3aec3e9c29d7b27c6f9cfa6bdb05
kiwi <242637385@qq.com>
2018-10-29 21:10:29 +0800

    修復採集來的文章樣式

--- static/js/custom_fix.js
+++ static/js/custom_fix.js
@@ -1,7 +1,8 @@
 $(function() {
     $('.index-middle').after($('.right_sidebar'));
     $('a[href^="http"]').not('a[href*=smwenku\\.com]').attr('target','_blank');
-    $('a[href^="http"]').not('a[href*=smwenku\\.com]').attr('target','_blank');
     $('.article-content pre.cpp').removeClass('cpp').addClass('language-cpp');
+    $('.article-content .dp-cpp').parent("pre").addClass('language-cpp');
+    Prism.highlightAll();
 });
 
