40e39bbe4509de0e992146489dc1adef3e4a7e2a
kiwi <242637385@qq.com>
2018-10-30 20:41:17 +0800

    当相关文章根据标签获取不足时 使用分类来填充

--- views/article.py
+++ views/article.py
@@ -9,15 +9,15 @@ from config import articles_per_page
 
 
 class ArticleHandler(BlockingHandler):
-    def related_sort(self,terms_id,related_posts):
+    def related_sort(self,terms_id,related_posts,related_type='tags'):
+        print(terms_id)
         orders = {}
         for i, term_id in enumerate(terms_id):
             orders[term_id] = i
-        print('ttt',terms_id,orders,related_posts)
         sort_key = {}
         for related_post in related_posts:
-            order_match_count = -sum(el in  terms_id for el in related_post['tags'])
-            order_first = min(orders[t] for t in related_post['tags'] if t in terms_id)
+            order_match_count = -sum(el in  terms_id for el in related_post[related_type])
+            order_first = min(orders[t] for t in related_post[related_type] if t in terms_id)
             sort_key[related_post['_id']] = (order_match_count,order_first)
         return sorted(related_posts, key=lambda x: sort_key[x['_id']])
 
@@ -26,13 +26,14 @@ class ArticleHandler(BlockingHandler):
         u_id = post['user']
         u = await self.application.db.users.find_one({"_id":ObjectId(u_id)})
         post['user'] = u
+        tags_id = [i for i in post['tags'] if i]
+        category_id = [i for i in post['category'] if i]
         category = []
         for c_id in post['category']:
             c = await self.application.db.terms.find_one({"_id":ObjectId(c_id)})
             category.append(c)
         post['category'] = category
         tags = []
-        tags_id = post['tags']
         #print(tags_id)
         for t_id in tags_id:
             t = await self.application.db.terms.find_one({"_id":ObjectId(t_id)})
@@ -46,18 +47,24 @@ class ArticleHandler(BlockingHandler):
         post_desc = ''.join([i.strip() for i in post_etree.xpath(".//text()")])[:200]
         #post_desc = post_etree.cssselect('p')[0].text
         post['desc'] = post_desc
-        print(post)
         menu_left = await self.application.db.menu.find({"type": "left"}).to_list(length=10)
         hot_posts = await sidebar.hot_posts(self.application.db)
-        #TODO
-        # 1.tags_id 为空时 不查询相关文章 并使用category_id 补充相关文章
-
         #related_posts =  await self.application.db.posts.find({'tags': {'$in': tags_id},'_id': {'$ne': post['_id']}}).sort([("views",-1)])\
             #.limit(articles_per_page).to_list(length=articles_per_page)
-        print(tags_id)
-        related_posts =  await self.application.db.posts.find({'tags': {'$in': tags_id},'_id': {'$ne': post['_id']}}) \
-            .limit(articles_per_page).to_list(length=articles_per_page)
-        related_posts = self.related_sort(tags_id,related_posts)
+        if tags_id:
+            related_posts =  await self.application.db.posts.find({'tags': {'$in': tags_id},'_id': {'$ne': post['_id']}}) \
+                .limit(articles_per_page).to_list(length=articles_per_page)
+            related_posts = self.related_sort(tags_id,related_posts,related_type='tags')
+        else:
+            related_posts = []
+        related_fill_num = articles_per_page - len(related_posts)
+        if related_fill_num > 0:
+            if category_id:
+                related_posts_category = await self.application.db.posts.find({'category': {'$in': category_id},'_id': {'$ne': post['_id']}}) \
+                .limit(related_fill_num).to_list(length=related_fill_num)
+                related_posts_category = self.related_sort(category_id, related_posts_category,related_type='category')
+                related_posts += related_posts_category
+        print(related_posts)
         related_posts = await join.post_user(related_posts, self.application.db)
         related_posts = await self.get_posts_desc(related_posts)
         self.set_header('cache-control',
