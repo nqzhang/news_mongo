3b29dcaefd2a178ab6d1497622500b3f95779218
kiwi <242637385@qq.com>
2018-11-05 21:16:51 +0800

    添加 user字典到模板 在模板可以使用user变量全部显示出来 {{ user }}
    现在的格式
    user = {"is_login":True,user_name:"hah"}
    is_login 是否登陆 True为登陆，False为没有登陆
    user_name 用户名
    当登陆时 这两个字段都会有
    当未登录时 只有is_login:False
    
    (cherry picked from commit 059ce44)

--- views/category.py
+++ views/category.py
@@ -10,9 +10,10 @@ from views.base import BaseHandler
 
 class CategoryPageHandler(BaseHandler):
     async def get(self,c_id,page=1):
+        user = await self.get_user()
         menu_left = await self.application.db.menu.find({"type": "left"}).to_list(length=10)
         posts =  await self.application.db.posts.find({"category":ObjectId(c_id)}).sort([("post_date",-1)]).skip(articles_per_page * (int(page) - 1)).limit(articles_per_page).to_list(length=articles_per_page)
         c_hot_posts = await sidebar.c_hot_posts(self.application.db,c_id)
         posts = await join.post_user(posts, self.application.db)
         #posts = await self.get_thumb_image(posts)
-        self.render('category.html',menu_left=menu_left,posts=posts,c_id=c_id,config=config,page=page,hot_posts=c_hot_posts)
+        self.render('category.html',menu_left=menu_left,posts=posts,c_id=c_id,config=config,page=page,hot_posts=c_hot_posts,user=user)
--- views/tag.py
+++ views/tag.py
@@ -8,8 +8,9 @@ from views.base import BaseHandler
 
 class TagPageHandler(BaseHandler):
     async def get(self,t_id,page=1):
+        user = await self.get_user()
         menu_left = await self.application.db.menu.find({"type": "left"}).to_list(length=10)
         posts =  await self.application.db.posts.find({"tags":ObjectId(t_id)}).sort([("post_date",-1)]).skip(articles_per_page * (int(page) - 1)).limit(articles_per_page).to_list(length=articles_per_page)
         posts = await join.post_user(posts, self.application.db)
         #posts = await self.get_thumb_image(posts)
-        self.render('tag.html',menu_left=menu_left,posts=posts,t_id=t_id,config=config,page=page)
+        self.render('tag.html',menu_left=menu_left,posts=posts,t_id=t_id,config=config,page=page,user=user)
