0f16ff53e17080df229abda347b05173786462ce
Duo Lu <kidder5142@gmail.com>
2018-11-08 23:42:31 +1100

    加固注册form邮箱检查功能

--- static/js/register.js
+++ static/js/register.js
@@ -1,41 +1,61 @@
 $(document).ready(function(){
+    var isEmailValid = false;
+
     $('#register-form').submit(function(e) {
         e.preventDefault();
-        $('.err-info').css('display', 'none')
 
         var email = $('#register-form input[name=email]').val();
         var passwd = $('#register-form input[name=passwd]').val();
         if (email === '' || passwd === '') {
-            $('.err-info').css('display', 'block')
+            showError('請輸入電子郵件和密碼');
         } else {
-            $('#register-form')[0].submit();
+            if (isEmailValid){
+                $('#register-form')[0].submit();
+            } else {
+                showError('郵件錯誤');                
+            }
         }
     });
 
-    // 访问该页面时，在url末尾添加?picid=8
-    // if ( getUrlParam('login') == 1 ) {
-    //     $('#login_sidebar').click();
-    // }
+    var emailField = $('#register-form input[name=email]')
+    // 检查邮件blur event
+    emailField.blur(function() {
+
+        //检查邮件是否为空
+        if (emailField.val() === '') {
+            isEmailValid = false;
+            return;
+        }
+
+        // 检查格式是否正确
+        if(!validateEmail(emailField.val())) {
+            isEmailValid = false;
+            showError('郵件格式錯誤');
+            return 
+        }
+
+        //检查是否邮件为空
+        $.post("/account/is_email_exist/" + emailField.val(), null, function(response) {
+            if (response == "郵箱已經註冊") { 
+                //特别处理
+                isEmailValid = false;
+                showError(response);
+            } else {
+                isEmailValid = true;
+                showError(response, false);            
+            }
+        });
+    });
 });
 
+function showError(errorMsg, red = true) {
+    if (!red) {
+        $('.err-info').css('color', '#3d3d3d');
+    }
+    $('.err-info').html(errorMsg);
+}
 
-// $(function() {
-//     var inputUserNameObj = $("input[name='email']");
-//     $("input[name='email']").blur(function() { //当该控件失去焦点时发生
-//         var text = inputUserNameObj.val(); //获得用户输入的邮箱
-//         $.post("/account/is_email_exist/" + text, null, function(response) { // 以POST方式跳转到action里面的方法中进行处理，并返回处理结果response
-//             if (response == "郵箱已經註冊") { //根据返回值进行处理
-//                 $("input[name='email']").focus(); //用户名输入控件获得焦点
-//                 document.getElementById("showResult").innerHTML = "<font color='red'>" + response + "</font>"; //在div中提示用户该用户名已经存在
-//             } else {
-//                 if ($("input[name='email']").val() == "") //判断用户名是否为空
-//                 {
-//                     document.getElementById("showResult").innerHTML = "<font color='red'>" + "郵箱不能為空" + "</font>"; // 在div中提示用户该用户名不能为空
-//                     $("input[name='email']").focus(); //用户名输入控件获得焦点
-//                 } else {
-//                     document.getElementById("showResult").innerHTML = "<font color='#90ee90'>" + response + "</font>";
-//                 }
-//             }
-//         });
-//     });
-// });
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
