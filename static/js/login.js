ca801b6b5df286be32086d4ea2d7fac02c6f4cd3
kiwi <242637385@qq.com>
2018-11-11 18:41:06 +0800

    登录页

--- static/js/login.js
+++ static/js/login.js
@@ -26,10 +26,9 @@ $(document).ready(function(){
         }
     });
 
-    // 访问该页面时，在url末尾添加?picid=8
-    // if ( getUrlParam('login') == 1 ) {
-    //     $('#login_sidebar').click();
-    // }
+if ( getUrlParam('login') == 1 ) {
+    $('a.login').click();
+    }
 });
 
 function toggleLoginLayer() {
@@ -41,15 +40,15 @@ function toggleLoginLayer() {
     }
 }
   
-// function getUrlParam(name){
-//     // 构造一个含有目标参数的正则表达式对象
-//     var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
-//     // 匹配目标参数
-//     var r = window.location.search.substr(1).match(reg);
-//     // 返回参数值
-//     if (r!=null) return unescape(r[2]);
-//     return null;
-// }
+function getUrlParam(name){
+     // 构造一个含有目标参数的正则表达式对象
+     var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+     // 匹配目标参数
+     var r = window.location.search.substr(1).match(reg);
+    // 返回参数值
+    if (r!=null) return unescape(r[2]);
+     return null;
+ }
 
 
 var _href = $("a#logout").attr("href");
