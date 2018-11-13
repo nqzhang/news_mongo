59be08a690b99bbd4174fb2ef54dc833540a70ae
kiwi <242637385@qq.com>
2018-11-13 15:54:11 +0800

    简书采集已修复不规则html 去除修复

--- static/js/custom_fix.js
+++ static/js/custom_fix.js
@@ -1,5 +1,4 @@
 $(function() {
-    $('.index-middle').after($('.right_sidebar'));
     $('a[href^="http"]').not('a[href*=smwenku\\.com]').attr('target','_blank');
     $('.article-content pre.cpp').removeClass('cpp').addClass('language-cpp');
     $('.article-content .dp-cpp').parent("pre").addClass('language-cpp');
