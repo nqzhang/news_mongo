cbe1340c63aff7a33e2987de4413cfa7bc6dee8e
Duo Lu <kidder5142@gmail.com>
2018-11-18 15:10:37 +1100

    1. 加入基本标签逻辑（完成） 2. 更新标签会动态更新表格数据，以便之后发送到后台（完成） 3. 默认显示文章已有的标签（完成） 4. 显示选项（个人分类里面有的那种） 5. 修复样式（完成）

--- static/js/tagsinput.js
+++ static/js/tagsinput.js
@@ -7,11 +7,8 @@
 */
 
 $(document).ready(function(){
-    // 这个变量用来检查点击新加按钮以后会不会增加新的标签输入栏，如果当前已经有一个空的标签栏，则不会添加新的
-    window.hasEmptyTag = {};
 
     $('input[data-role=tagsinput]').each(function(index, element){
-        window.hasEmptyTag[index] = false;
         init(index, element);
     });
 });
@@ -51,13 +48,11 @@ function renderAddTagButton(index, element) {
     var $addTagButton = $('<div class="add-more-tags"><img src="/static/svgs/add-more.svg" />添加新' + $(element).data('display-name') + '</div>')
         .on('click', function() {
             //没有空标签时候才会添加新标签
-            if (!window.hasEmptyTag[index]) {
+            if (!hasEmptyTag(index)) {
                 // add new tag into DOM
                 $($('.tag-section')[index]).children('.tags').append(renderTag(index));
-                // update global variable
-                window.hasEmptyTag[index] = true;
                 // focus on the new tag
-                $($('.tag-section')[index]).find('.tags input.tag-input:last').focus();
+                $($('.tag-section')[index]).find('.tags .tag-input:last').focus();
             }
         })
 
@@ -66,49 +61,29 @@ function renderAddTagButton(index, element) {
 
 function renderTag(index, isNew = true, tagIndex = null, value = null) {
     var preVal;
-    var _tagIndex;
-    if (isNew) {
-        _tagIndex = $($('.tag-section')[index]).find('.tags .tag-wrapper').length ;
-    } else {
-        _tagIndex = tagIndex;
-    }
 
     //删除按钮
     var closeButton = $('<span class="tag-close">╳</span>')
         .click(function(){
-            //如果删除了空标签，设置hasEmptyTag为false， 这样就可以允许用户再继续添加标签
-            if($(this).prev().val() === "") {
-                window.hasEmptyTag[index] = false
-            }
             $(this).parent().remove();
             updateInputValue(index);
         });
 
     //标签
-    var $tag = $('<input />', {
+    var $tag = $('<span />', {
             'class': 'tag-input',
-            'type': 'text',
-            'max-length': '50'})
-        .change(function() {
-            //判定是否需要改变hasEmptyTag变量
-            if($(this).val() === '' ) {
-                window.hasEmptyTag[index] = true;
-            } else {
-                if (window.hasEmptyTag[index] === false || preVal === '') {
-                    window.hasEmptyTag[index] = false;
-                }
-            }
+            'contenteditable': true
         })
         .on('input', function() {
             //更新真正input的value
             updateInputValue(index);
         })
         .focus(function() {
-            preVal = $(this).val();
+            preVal = $(this).text();
         });
     
     if (!isNew) {
-        $($tag).val(value);
+        $($tag).text(value);
     }
 
     var wrapper = $('<div class="tag-wrapper"></div>')
@@ -122,10 +97,23 @@ function updateInputValue(index) {
     var $inputElement = $('input[data-role=tagsinput]').eq(index);
     var tagsArr = [];
     $inputElement.parent().find('.tag-wrapper .tag-input').each(function(index, tagElement){
-        tagsArr.push($(tagElement).val());
+        tagsArr.push($(tagElement).text());
     });
 
     $($inputElement).val(tagsArr.join());
+}
+
+function hasEmptyTag(index) {
+    var $inputElement = $('input[data-role=tagsinput]').eq(index);
+    
+    var hasEmptyTag = false
+    $inputElement.parent().find('.tag-wrapper .tag-input').each(function(index, tagElement){
+        if($(tagElement).text() === '') {
+            hasEmptyTag = true;
+        }
+    });
+
+    return hasEmptyTag;
 }
 
 function renderOptions() {
