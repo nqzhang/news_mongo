bae1e7ff85bb19e386db899d7169b2e803cb551f
kiwi <242637385@qq.com>
2018-11-19 11:18:42 +0800

    將代碼高亮換成 highlight.js

--- static/js/custom_fix.js
+++ static/js/custom_fix.js
@@ -2,6 +2,6 @@ $(function() {
     $('a[href^="http"]').not('a[href*=smwenku\\.com]').attr('target','_blank');
     $('.article-content pre.cpp').removeClass('cpp').addClass('language-cpp');
     $('.article-content .dp-cpp').parent("pre").addClass('language-cpp');
-    Prism.highlightAll();
+    //Prism.highlightAll();
 });
 
