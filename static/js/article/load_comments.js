b454dc3367b3061073bdb53bf4a6c23d78d23b41
Duo Lu <kidder5142@gmail.com>
2019-01-03 23:25:36 +1100

    发布留言，显示留言列表初版，表格还没有功能只是个样子

--- static/js/article/load_comments.js
+++ static/js/article/load_comments.js
@@ -4,8 +4,27 @@ function ApiCommentsGetAll() {
         headers: {'X-XSRFToken': token},
         data: {post_id: post_id},
         type: "POST",
-        success: function (data, textStatus, jqXHR) {
-            console.log(data);
+        success: function (res, textStatus, jqXHR) {
+            var data = JSON.parse(res);
+            if(data.length > 0) {
+                $('#comment-form-wrapper').after("<div id='article-comments'><div class='section-head'>所有留言</div></div>");
+
+                data.forEach(function(comment){
+                    $('#article-comments').append(
+                        "<div class='comment-item'>" + 
+                            "<div class='comment-content'>" +
+                                "<div class='comment-author'>" + comment.comment_author_name.charAt(0) + "</div>" +
+                                "<a href=''>" + comment.comment_author_name + "</a>" +
+                                "<div>" + comment.comment_content + "</div>" +
+                            "</div>" + 
+                            "<div class='comment-meta'>" + 
+                                "<img src='/static/svgs/clock.svg' />" +
+                                "<div>" + comment.comment_date.$date + "</div>" +
+                            "</div>" + 
+                        "</div>"
+                    )
+                });
+            }
         }
     });
 }
