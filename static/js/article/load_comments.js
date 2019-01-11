a53882b0d939819fc8e11ea76d415cd2e17114af
kiwi <242637385@qq.com>
2019-01-11 10:56:10 +0800

    后台时间为utc时间 使用前需要转换成 本地时间戳 时间显示参考disqus 节约空间

--- static/js/article/load_comments.js
+++ static/js/article/load_comments.js
@@ -66,7 +66,7 @@ function ApiCommentsGetAll() {
                                     "<a class='author-name' href='/u/" + comment.comment_author_id.$oid + "' target='_blank'>" + comment.comment_author_name + "</a>" +
                                     "<div class='comment-time'>" +
                                         "<img src='/static/svgs/clock.svg' />" +
-                                        "<div>" + getDateTime(comment.comment_date.$date) + "</div>" +
+                                        "<div>" + "<a title=" + "'" + getDateTime(comment.comment_date.$date) + "'>"  + timeago(comment.comment_date.$date) + "</a>" + "</div>" +
                                     "</div>" +
                                 "</div>" +
                                 "<div class='reply-comment' data-reply-to='" + comment._id.$oid + "' data-author-name='" + comment.comment_author_name + "'>" +
@@ -113,9 +113,16 @@ function ApiCommentsGetAll() {
     });
 }
 
-function getDateTime(dateInMillionSecond) {
-    var date = new Date(dateInMillionSecond);
-    return date.toISOString().slice(0, 10) + ' ' + date.getHours() + ':' + date.getMinutes();
+function toLocalTimestamp(utcTimestamp){
+    localTimestamp = utcTimestamp + new Date().getTimezoneOffset()*60*1000;
+    return localTimestamp;
+}
+function getDateTime(utcTimestamp) {
+    var localTimestamp = toLocalTimestamp(utcTimestamp);
+    var date = new Date(localTimestamp);
+    var hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+    var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+    return date.toISOString().slice(0, 10) + ' ' + hour + ":" + minutes;
 }
 
 function sortDataByDate(a, b) {
@@ -158,5 +165,50 @@ function extractRepliedContent(content){
     } else {
         return content;
     }
+}
+
+
+function timeago(dateTimeStamp){   //dateTimeStamp是一个时间毫秒，注意时间戳是秒的形式，在这个毫秒的基础上除以1000，就是十位数的时间戳。13位数的都是时间毫秒。
+    var minute = 1000 * 60;      //把分，时，天，周，半个月，一个月用毫秒表示
+    var hour = minute * 60;
+    var day = hour * 24;
+    var week = day * 7;
+    var halfamonth = day * 15;
+    var month = day * 30;
+    var now = new Date().getTime();   //获取当前时间毫秒
+    var diffValue = now - toLocalTimestamp(dateTimeStamp);//时间差
+    console.log(diffValue);
+    if(diffValue < 0){
+        return;
+    }
+    var minC = diffValue/minute;  //计算时间差的分，时，天，周，月
+    var hourC = diffValue/hour;
+    var dayC = diffValue/day;
+    var weekC = diffValue/week;
+    var monthC = diffValue/month;
+    if(monthC >= 1 && monthC <= 3){
+        result = " " + parseInt(monthC) + "月前"
+    }else if(weekC >= 1 && weekC <= 3){
+        result = " " + parseInt(weekC) + "周前"
+    }else if(dayC >= 1 && dayC <= 6){
+        result = " " + parseInt(dayC) + "天前"
+    }else if(hourC >= 1 && hourC <= 23){
+        result = " " + parseInt(hourC) + "小时前"
+    }else if(minC >= 1 && minC <= 59){
+        result =" " + parseInt(minC) + "分钟前"
+    }else if(diffValue >= 0 && diffValue <= minute){
+        result = "刚刚"
+    }else {
+        var datetime = new Date();
+        datetime.setTime(dateTimeStamp);
+        var Nyear = datetime.getFullYear();
+        var Nmonth = datetime.getMonth() + 1 < 10 ? "0" + (datetime.getMonth() + 1) : datetime.getMonth() + 1;
+        var Ndate = datetime.getDate() < 10 ? "0" + datetime.getDate() : datetime.getDate();
+        var Nhour = datetime.getHours() < 10 ? "0" + datetime.getHours() : datetime.getHours();
+        var Nminute = datetime.getMinutes() < 10 ? "0" + datetime.getMinutes() : datetime.getMinutes();
+        var Nsecond = datetime.getSeconds() < 10 ? "0" + datetime.getSeconds() : datetime.getSeconds();
+        result = Nyear + "-" + Nmonth + "-" + Ndate
+    }
+    return result;
 }
 
