c289f27e51ad8a081ebc3943268f215ed33cfb1f
kiwi <242637385@qq.com>
2019-01-28 21:42:37 +0800

    增加静态文件在cloudflare的缓存时间

--- views/static.py
+++ views/static.py
@@ -14,4 +14,4 @@ class StaticFileHandler(tornado.web.StaticFileHandler):
     def set_extra_headers(self, path):
         #if path == 'sitemap.xml':
         self.set_header('cache-control',
-                            'public, stale-while-revalidate=120,stale-if-error=3600,max-age=600,s-maxage=86400')
+                            'public, stale-while-revalidate=120,stale-if-error=3600,max-age=600,s-maxage=86400000')
