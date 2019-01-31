c3bd12e27e5961cfa7cc75cd084081e14ff8a0e8
kiwi <242637385@qq.com>
2019-01-31 18:03:13 +0800

    写错了

--- views/api.py
+++ views/api.py
@@ -15,7 +15,7 @@ class ApiListHandler(RequestHandler):
             self.render('component/list-page/content-list.html', posts=posts)
         if path=="tag":
             t_id = self.get_argument('id')
-            posts = await self.application.db.posts.find({"_id":{"$lt":ObjectId(postoffset)},"tags": ObjectId(c_id)}).sort([("post_date", -1)]).limit(articles_per_page).to_list(length=articles_per_page)
+            posts = await self.application.db.posts.find({"_id":{"$lt":ObjectId(postoffset)},"tags": ObjectId(t_id)}).sort([("post_date", -1)]).limit(articles_per_page).to_list(length=articles_per_page)
             posts = await join.post_user(posts,self.application.db)
             self.render('component/list-page/content-list.html', posts=posts)
 
