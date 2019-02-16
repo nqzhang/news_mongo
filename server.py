b452b33a8b61dbfb6cf56be85f10b46ab1ae27bc
kiwi <242637385@qq.com>
2019-02-17 07:16:43 +0800

    平滑重启

--- server.py
+++ server.py
@@ -9,7 +9,8 @@ from apscheduler.schedulers.tornado import TornadoScheduler
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from datetime import datetime
 from cron import cron
-import asyncio
+import asyncio,time,logging,signal
+from functools import partial
 from tornado.platform.asyncio import AsyncIOMainLoop
 try:
     import uvloop
@@ -17,6 +18,30 @@ try:
     AsyncIOMainLoop().install()
 except:
     pass
+
+MAX_WAIT_SECONDS_BEFORE_SHUTDOWN = 3
+
+def sig_handler(server, sig, frame):
+    io_loop = tornado.ioloop.IOLoop.instance()
+
+    def stop_loop(deadline):
+        now = time.time()
+        if now < deadline and (io_loop._callbacks or io_loop._timeouts):
+            logging.info('Waiting for next tick')
+            io_loop.add_timeout(now + 1, stop_loop, deadline)
+        else:
+            io_loop.stop()
+            logging.info('Shutdown finally')
+
+    def shutdown():
+        logging.info('Stopping http server')
+        server.stop()
+        logging.info('Will shutdown in %s seconds ...',
+                     MAX_WAIT_SECONDS_BEFORE_SHUTDOWN)
+        stop_loop(time.time() + MAX_WAIT_SECONDS_BEFORE_SHUTDOWN)
+
+    logging.warning('Caught signal: %s', sig)
+    io_loop.add_callback_from_signal(shutdown)
 
 if __name__ == "__main__":
     #asyncio.set_event_loop(asyncio.new_event_loop())
@@ -28,8 +53,11 @@ if __name__ == "__main__":
     #scheduler.add_job(cron.count_category, 'date', run_date=datetime(2018, 8, 17, 4, 54, 0))
     #scheduler.start()
     app = Application(db)
-    app.listen(48000)
+    app.listen(sys.argv[1])
+    signal.signal(signal.SIGTERM, partial(sig_handler, app))
+    signal.signal(signal.SIGINT, partial(sig_handler, app))
     loop = asyncio.get_event_loop()
     app.init_with_loop(loop)
     #loop.set_blocking_log_threshold(0.5)
     loop.run_forever()
+
