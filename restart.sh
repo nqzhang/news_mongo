9687a9e2e26a3fb540e9292a4db0914d05db5473
kiwi <242637385@qq.com>
2019-02-17 07:31:51 +0800

    平滑重启

--- restart.sh
+++ restart.sh
@@ -1,1 +1,1 @@
-for x in `supervisorctl status | grep www.smwenku.com | awk '{print $1}'`;do supervisorctl restart $x;done
+for x in `supervisorctl status | grep www.smwenku.com | awk '{print $1}'`;do supervisorctl restart $x;sleep 5;done
