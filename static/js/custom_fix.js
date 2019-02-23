ac41f4da45364af2e13161884259aae913ca14ad
kiwi <242637385@qq.com>
2019-02-23 15:24:08 +0800

    51cto防盗链

--- static/js/custom_fix.js
+++ static/js/custom_fix.js
@@ -3,5 +3,10 @@ $(function() {
     $('.article-content pre.cpp').removeClass('cpp').addClass('language-cpp');
     $('.article-content .dp-cpp').parent("pre").addClass('language-cpp');
     //Prism.highlightAll();
+    $('.article-content img[src*="51cto.com"]').each(function(i, e) {
+	$(e).attr('referrerpolicy', 'no-referrer');
+	$(e).attr("src", $(e).attr("src"));
+    })
 });
+
 
