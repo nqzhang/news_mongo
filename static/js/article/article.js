3242440fc6fdb09809b2e8e512b7d94815ec3e0a
kiwi <242637385@qq.com>
2019-04-10 23:58:22 +0800

    修復後台發郵件不顯示 增加評論提醒

--- static/js/article/article.js
+++ static/js/article/article.js
@@ -1,9 +1,10 @@
 $(window).on("load",function () {
-    var comment_id = getUrlParam('commentScrool');
-    commentScrool_selector=   '[data-comment-id="' + comment_id + '"]';
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $(commentScrool_selector).offset().top
-    }, 300);
-
+        var comment_id = getUrlParam('commentScrool');
+        if (comment_id) {
+            commentScrool_selector = '[data-comment-id="' + comment_id + '"]';
+            $([document.documentElement, document.body]).animate({
+                scrollTop: $(commentScrool_selector).offset().top
+            }, 300);
+        }
     }
 )
